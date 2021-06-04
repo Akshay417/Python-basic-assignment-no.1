64f8abc28318e11c68329ccfa8dd64aa12a70bab
Akshay Chavan <81507357+Akshay417@users.noreply.github.com>
2021-06-04 19:10:09 +0530

    Add files via upload

---
+++ Untitled68.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+Python basic assignment no.3Q1. Why are functions advantageous to have in your programs?
+Ans:Functions are use for the repeatative typing of code for the execution, and also function reduces length of code. we can execute many times same code for the same output.  Q2. When does the code in a function run: when it's specified or when it's called?
+Ans:when its called then the code in a function run.Q3. What statement creates a function?
+Ans:"def" keyword is a statment creates a function.Q4. What is the difference between a function and a function call? 
+Ans:'function' is a procedure to achive a perticular task, and 'function call' is use to execute that perticular procedure for result.Q5. How many global scopes for the python program? how many local scopes?
+Ans:There are only one global scope for the python program. and there are four local scopes for python program.Q6. What happens to variables in a local scope when a function call returns?
+Ans:Variables in a local scope get destroyed when a function call returns.Q7. What is a concept of return value? it is possible to have return value in an expression?
+Ans:Return value is a concept in which return is a value of a function to which call the function when its complete its task.Q8. if function have does not have return statment,what is a return value of a call to that function?
+Ans:if function have does not have return statment, then return value of call to that function is none. Q9. How do you make a function variable refer to the globle variable ?
+Ans:for making fuction variable to the globle variable we have to use "globle" keyword.Q10. What is a data type of none?
+Ans:data type of none represent null value. Q11. what does the sentence import areallyourpetsnamederic do?
+Ans:sentence import areallyourpetsnamederic, import module having name areallyourpetsnamederic. Q12. if you had bacon()feature in spam module,what would you call it after importing a spam ?
+Ans:i would call it after importing spam,   spam.bacon().Q13. What can you do to save a programme from crashing if it encounters an error ?
+Ans:to save a programme from crashing if it encounters an error then error handing can be used to notify to user that why error is occured. Q14. what is a purpose of try clause? what is a purpose of except clause?
+Ans:try clause allow you to define block of code to be tested for error while it is being executed. except clause is use to combine two Select statments and return rows from first select statment that are not return by second select statment.  
+Thank you..!
+# In[ ]:
+
+
+
+
